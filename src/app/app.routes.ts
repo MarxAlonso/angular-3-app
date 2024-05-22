5e46936f4d1d0a64d3006da2fb4a1e988344092c
Marx Alonso <marxchip99@gmail.com>
2024-05-22 09:25:19 -0500

    v1 del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {ContactComponent} from './contact/contact.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import { CompanyComponent } from './company/company.component';
+import { EmployeesComponent } from './employees/employees.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //Aca se cambia el titulo de la pagina dependiendo del apartado de la pagina que nos estamos dirigiendo
+    {'path': 'home', 'title': 'Inicio', component:HomeComponent},
+    {'path': 'about', 'title': 'Sobre Nosotros', component:AboutComponent},
+    {'path': 'about', children:[
+        {'path': 'company', 'title': 'Sobre Nosotros | Company', component: CompanyComponent},
+        {'path': 'employees', 'title': 'Sobre Nosotros | Employees', component: EmployeesComponent}
+    ]},
+    {'path': 'contact', 'title': 'Contacto', component:ContactComponent},
+    {'path': '', redirectTo: '/home', pathMatch: 'full'},
+    {'path': '**', component:PageNotFoundComponent},
+];
---
+++ src/app/employees/employees.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+@Component({
+  selector: 'app-employees',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './employees.component.html',
+  styleUrl: './employees.component.css'
+})
+export class EmployeesComponent {
+
+}
